e28f8e960bbae2165f1dea4c993fbf0761f1e710
Jason Frame <jason@onehackoranother.com>
2014-02-04 11:47:32 +0000

    initial

---
+++ index.js
@@ -0,0 +1,45 @@
+exports.isString = function(thing) {
+	return typeof thing === 'string';
+}
+
+exports.isNumber = function(thing) {
+	return typeof thing === 'number';
+}
+
+exports.isInteger = function(thing) {
+	return typeof thing === 'number' && ((thing | 0) === thing);
+}
+
+exports.isBoolean = function(thing) {
+	return typeof thing === 'boolean';
+}
+
+exports.isArray = function(thing) {
+	return Array.isArray(thing);
+}
+
+exports.isFunction = function(thing) {
+	return typeof thing === 'function';
+}
+
+exports.isNull = function(thing) {
+	return thing === null;
+}
+
+exports.isNotNull = function(thing) {
+	return thing !== null;
+}
+
+exports.isObject = function(thing) {
+	return (typeof thing === 'object')
+		&& (thing !== null)
+		&& (!Array.isArray(thing));
+}
+
+exports.isDefined = function(thing) {
+	return typeof thing !== 'undefined';
+}
+
+exports.isUndefined = function(thing) {
+	return typeof thing === 'undefined';
+}
